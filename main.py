998278c8f004d2c1f9274f0fbd2f50a0e834d1c9
Wenqing Zong <wenqing.zong98@gmail.com>
2025-02-09 03:59:12 +0800

    Logger and config setup

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
+import colorlog
+import requests
+import yaml
+
+
+def load_config(config_file="config.yaml"):
+    try:
+        with open(config_file, "r") as file:
+            return yaml.safe_load(file)
+    except Exception as e:
+        LOGGER.error("Error loading configuration file: " + str(e))
+        exit(1)
+
+
+# Global variables
+# LOGGER setup
+LOGGER = colorlog.getLogger()
+LOGGER.setLevel(colorlog.DEBUG)
+handler = colorlog.StreamHandler()
+formatter = colorlog.ColoredFormatter(
+    "%(log_color)s%(asctime)s - %(levelname)s - %(message)s",
+    datefmt=None,
+    reset=True,
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red,bg_white",
+    },
+    secondary_log_colors={},
+    style="%",
+)
+handler.setFormatter(formatter)
+LOGGER.addHandler(handler)
+CONFIG = load_config()
+
+
 def main():
-    print("Hello from cryptotest!")
+    LOGGER.info(CONFIG)
 
 
 if __name__ == "__main__":
